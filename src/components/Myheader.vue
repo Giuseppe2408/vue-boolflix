305a5b55478cdfc045f4c13073edcdb43f00a214
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-06-15 15:25:56 +0200

    fine primo milestone

--- src/components/Myheader.vue
+++ src/components/Myheader.vue
@@ -5,6 +5,7 @@
         <img src="https://media-assets.wired.it/photos/615ef4b55ccc3b73fb14d5b2/master/pass/wired_placeholder_dummy.png" alt="logo netflix">  
         <span>{{filmUser}} </span>    
         <input 
+        placeholder="search"
         v-model="userTxt" 
         type="search">
         <button @click="$emit('mysearch', userTxt)">cerca</button>
